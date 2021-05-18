c4235f9dfb08c041f8bf1e6db85c53130e98ccb7
Navya Vijayan <navyavijayan742000@gmail.com>
2021-05-18 13:33:32 +0530

    Product listing and Bookmark

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
+import 'package:shoppingapp/screens/landing_page.dart';
 import 'package:flutter/material.dart';
-import './root_page.dart';
-import './auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Shopify',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        accentColor: Color(0xFFFF1E00)
       ),
-      home: new RootPage(auth: new Auth()),
+      home: LandingPage(),
     );
   }
 }
+
